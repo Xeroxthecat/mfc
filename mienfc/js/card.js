d3e50dc5d3ddb4cb2dce75f24ed261f8ff8e0d85
Xerox <35755435+Xeroxthecat@users.noreply.github.com>
2018-09-17 22:15:35 -0700

    flip to next word in the array

--- mienfc/js/card.js
+++ mienfc/js/card.js
@@ -4,76 +4,85 @@
 
     //var url = 'http://simplemike.com/sandbox/test.json';
     var url = "http://www.json-generator.com/api/json/get/cqgtPFKIRK?indent=2";
-
-    //display variables
-    var picture = "";
-    var soundFile = "";
-    var wordEnglish = "";
-    var wordMien = "";
-
-    //tracker variables
     var front = true;
     var arrayIndex = 0;
+    var myObj = '';
+    var category = '';
 
     //Colors
     function getColors() {
       fetch(url)
         .then(function (data) {
+       
           return data.json();
         })
         .then(function (myJson) {
           console.log(JSON.stringify(myJson));
-          console.log(myJson[0].animals[arrayIndex].englishWord);
-  
-          picture = myJson[0].colors[arrayIndex].image;
-          soundFile = myJson[0].colors[arrayIndex].soundFile;
-          wordEnglish = myJson[0].colors[arrayIndex].englishWord;
-          wordMien = myJson[0].colors[arrayIndex].mienWord;
+          //console.log(myJson[0].animals[arrayIndex].englishWord);
 
-          displayTop(picture);
-          displayBottom(wordEnglish);
+          //display the card for the first time
+          displayPic(myJson, arrayIndex);
+          displayEngWord(myJson, arrayIndex);
 
+          myObj = myJson;
+          return myJson;
         })
         .catch(function (error) {
           console.log("An error occurred: ", error.message);
         });
     }
 
-    function displayTop(y) {
-      cardTop.innerHTML = JSON.stringify(y);
+    function displayPic(obj, index) {
+      cardTop.innerHTML = JSON.stringify(obj[0].colors[index].image);
     }
 
-    function displayBottom(x) {
-      cardBottom.innerHTML = JSON.stringify(x);
+    function displayEngWord(obj, index) {
+      cardBottom.innerHTML = JSON.stringify(obj[0].colors[index].englishWord);
+    }
+
+    function displayMienWord(obj, index) {
+      cardTop.innerHTML = JSON.stringify(obj[0].colors[index].mienWord);
+    }
+
+    function displaySound(obj, index) {
+      cardBottom.innerHTML = JSON.stringify(obj[0].colors[index].soundFile);
     }
 
     function flipCard() {
+
+      //check if within array, if not, loop back to front
+      if (arrayIndex >= (myObj[0].colors.length)) {
+        arrayIndex = 0;
+      }
+
       if (front) {
-        displayTop(wordMien);
-        displayBottom(soundFile);
+        displayMienWord(myObj, arrayIndex);
+        displaySound(myObj, arrayIndex);
         front = false;
         document.getElementById('flashcard').style.backgroundColor = "silver";
+
       }
       else {
-        displayTop(picture);
-        displayBottom(wordEnglish);
+        displayPic(myObj, arrayIndex);
+        displayEngWord(myObj, arrayIndex);
         front = true;
         document.getElementById('flashcard').style.backgroundColor = "white";
       }
+
     }
-    
-    function nextCard(){
+
+    function nextCard() {
       console.log("on to the next card");
       arrayIndex++;
       console.log('array index is: ' + arrayIndex)
     }
 
-    function answeredCorrect(){
+    function answeredCorrect() {
       console.log("i got this right");
       nextCard();
     }
-    
-    function answeredWrong(){
+
+    function answeredWrong() {
       console.log("i got this wrong");
       nextCard();
     }
